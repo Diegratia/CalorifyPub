75ebed9db210de88e2e6681ab78bfec5a3edaa2a
Diegratia <d.febrian2002@gmail.com>
2023-06-10 09:09:21 +0700

    indexUpdate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 const apiRoutes = require('./routes/api-routes');
 const usersRoutes = require('./routes/users-routes');
-const {registerMiddleware, loginMiddleware, logoutMiddleware, forgotPassword} = require('./routes/auth-routes');
+const {registerMiddleware, loginMiddleware, logoutMiddleware, forgotPassword, registerData, checkEmail} = require('./routes/auth-routes');
 const {checkAuth} = require('./routes/authlogin-routes');
 
 const app = express();
@@ -14,12 +14,16 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+
 app.use('/auth/register', registerMiddleware);
+app.use('/auth/registerdata' ,checkAuth, registerData);
 app.use('/auth/login',loginMiddleware);
 app.use('/auth/logout', logoutMiddleware);
 app.use('/auth/forgotpassword', forgotPassword);
-
+app.use('/auth/checkemail', checkEmail);
 app.use('/middleware/authlogincheck', checkAuth);
+
+
 
 app.use('/user', checkAuth, usersRoutes.routes);
 app.use('/api', checkAuth, apiRoutes.routes);
