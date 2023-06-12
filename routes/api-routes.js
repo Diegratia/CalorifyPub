9f7d8d48d9ca1eec0115a586cba00503b14e1c6c
Diegratia <d.febrian2002@gmail.com>
2023-06-12 22:17:49 +0700

    delete news api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const { getBMIbyId } = require('../controller/api/bmiController');
 const { getBMRbyId } = require('../controller/api/bmrcontroller');
-const {getNews} = require('../controller/api/newsController');
 const {postNutrition, getNutrition, deleteNutrition, getTotalCalories} = require('../controller/api/nutritionController');
 const router = express.Router();
 
 router.get('/bmibyid/:id', getBMIbyId);
 
 router.get('/bmrbyid/:id', getBMRbyId);
-router.get('/news', getNews);
 
 // router.get('/nutrition', getNutrition);
 router.post('/nutrition', postNutrition);
